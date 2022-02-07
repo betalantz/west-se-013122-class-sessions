9a0c4f873ac2b1f449a35c59b12db5ae2d9dbb78
betalantz <betalantz@gmail.com>
2022-02-07 09:28:58 -0800

    creates branch for DQ Events

--- 01-js-dom-setup/src/index.js
+++ 01-js-dom-setup/src/index.js
@@ -1,13 +1,44 @@
-const cat1 = {
-    name: "Simon",
-    age: 9,
-    job: "tuna terrorist"
+// Ex 1
+
+let whatHappened = document.querySelector("#happening");
+
+const btn = document.getElementById("notify")
+btn.addEventListener('click', (e) => eventHandler(e, "clicked"))
+
+// Ex 2
+
+// const btn = document.getElementById("notify")
+btn.addEventListener('mouseover', (e) => eventHandler(e, "moused"))
+
+function eventHandler(event, type){
+    // console.log('event: ', event);
+    console.log(`the use just ${type}`);
+    whatHappened.innerText = type
+    type == "clicked" ? whatHappened.style.color = "blue" : whatHappened.style.color = "green"
 }
-const cat2 = "Miyuki"
-const error = "Cat not found!"
-console.log("Hello world!");
-console.log("My cats: ", cat1, cat2, "are the cutest!");
-// debugger
-console.error(error)
-console.dir({cat1})
-console.table(cat1)
+
+console.log(whatHappened)
+
+
+// Ex 3
+// script tag to high on html w/o defer
+
+// Ex 4
+
+// index.js
+// document.addEventListener("DOMContentLoaded", function(){
+//     const btn = document.getElementById("notify")
+//     btn.addEventListener('mouseover', function(){
+//       console.log("Printing a Message!")
+//     })
+//   });
+
+// Ex 5
+
+// index.js
+// document.addEventListener("DOMContentLoaded", function(){
+//     const btn = document.getElementById("notifiable")
+//     btn.addEventListener('click', function(){
+//       console.log("Printing a Message!")
+//     })
+//   });

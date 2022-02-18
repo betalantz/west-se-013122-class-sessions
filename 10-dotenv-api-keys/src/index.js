6b3a1704c24f22a4500dc3b7bb632d13bee4ff1b
betalantz <betalantz@gmail.com>
2022-02-18 10:41:27 -0800

    adds parcel and dotenv to 10

--- 10-dotenv-api-keys/src/index.js
+++ 10-dotenv-api-keys/src/index.js
@@ -1,8 +1,11 @@
+// import 'dotenv/confg'
+require('dotenv').config()
+
 fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?timeFrame=day&targetCalories=2000&diet=vegetarian&exclude=shellfish%2C%20olives", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-		"x-rapidapi-key": "3d001f9d8emshad8baf330954b72p18c7d7jsnd54d7284750d"
+		"x-rapidapi-key": process.env.RAPIDAPI_KEY
 	}
 })
 .then(response => {

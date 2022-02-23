baf5428d34668bbde29906027f7494b229b791b7
betalantz <betalantz@gmail.com>
2022-02-23 09:38:41 -0800

    adds bonus delivs to 11

--- 11-dq-components/src/components/AlbumList.js
+++ 11-dq-components/src/components/AlbumList.js
@@ -3,9 +3,26 @@ import AlbumCard from "./AlbumCard";
 import { albums } from "../data/albums";
 
 function AlbumList() {
+  console.log(albums)
+  const albumCards = albums.map(albumObj => {
+    return <AlbumCard 
+      key={albumObj.id}
+      name={albumObj.name}
+      image={albumObj.image}
+      genre={albumObj.genre}
+    />
+  })
   return (
     <section className="albums">
-      <AlbumCard
+      {albumCards}
+      {/* {albums.map(albumObj => {
+    return <AlbumCard 
+      name={albumObj.name}
+      image={albumObj.image}
+      genre={albumObj.genre}
+    />
+    })} */}
+      {/* <AlbumCard
         name="Classic Tuna Cans Being Opened"
         image="https://target.scene7.com/is/image/Target/GUEST_360eeed2-1747-4b15-8394-a22c7cd12230?wid=488&hei=488&fmt=pjpeg"
         genre="Cats"
@@ -24,7 +41,7 @@ function AlbumList() {
         name="Aloha K.K."
         image="https://vignette.wikia.nocookie.net/animalcrossing/images/5/5d/NH-Album_Cover-Aloha_K.K..png/revision/latest?cb=20200502231742"
         genre="Dogs"
-      />
+      /> */}
     </section>
   );
 }

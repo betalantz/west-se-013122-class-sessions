95427ffff8b66956e85ddd21968875eef56845b5
betalantz <betalantz@gmail.com>
2022-02-24 14:59:58 -0800

    adds solution for Palindrome 1

--- algo-club/phase-1-algorithms-palindrome/index.js
+++ algo-club/phase-1-algorithms-palindrome/index.js
@@ -1,9 +1,34 @@
+/* what's the algorithm problem in our own words
+write a funtion which read a string and compares itself forwards and backwards
+returns true if the string is the same forwards and backward, and returns false if it isnt'
+*/
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // convert the string to array
+  // const wordArr = Array.from(word)
+  // reverse the array
+  // const reversed = wordArr.reverse()
+  // convert back to string
+  // const backWord = reversed.join("")
+
+  // compare the reversed string to the orignal string
+  // return boolean of the comparison
+  return word == reverseString(word)
+}
+
+function reverseString(string){
+  const wordArr = Array.from(string)
+  const reversed = wordArr.reverse()
+  return reversed.join("")
 }
 
 /* 
   Add your pseudocode here
+convert the string to array
+reverse the array
+convert back to string
+compare the reversed string to the orignal string
+return boolean of the comparison
 */
 
 /*
@@ -15,6 +40,9 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
 
   console.log("");
 

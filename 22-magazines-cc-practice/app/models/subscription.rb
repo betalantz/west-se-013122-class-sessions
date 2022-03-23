1d3d6ecbb5b938eb83d76b0e3ba56b5273e1acc4
betalantz <betalantz@gmail.com>
2022-03-23 08:49:14 -0700

    completes Subscription model

--- 22-magazines-cc-practice/app/models/subscription.rb
+++ 22-magazines-cc-practice/app/models/subscription.rb
@@ -2,5 +2,9 @@ class Subscription < ActiveRecord::Base
 
     belongs_to :reader
     belongs_to :magazine
+
+    def print_details
+        puts "#{self.reader.name} subscribed to #{self.magazine.title} for $#{self.price}"
+    end
   
 end

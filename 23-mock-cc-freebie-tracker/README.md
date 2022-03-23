dc46d567eacb2e191f5dc884a501b17cafd100b1
betalantz <betalantz@gmail.com>
2022-03-23 15:31:12 -0700

    complete challenge

--- 23-mock-cc-freebie-tracker/README.md
+++ 23-mock-cc-freebie-tracker/README.md
@@ -162,10 +162,10 @@ companies for the first dev in the database based on your seed data; and
 
 #### Dev
 
-- [] `Dev#received_one?(item_name)`
+- [x] `Dev#received_one?(item_name)`
   - accepts an `item_name` (string) and returns true if any of the freebies
     associated with the dev has that `item_name`, otherwise returns false
-- [] `Dev#give_away(dev, freebie)`
+- [x] `Dev#give_away(dev, freebie)`
   - accepts a `Dev` instance and a `Freebie` instance, changes the freebie's dev
     to be the given dev; your code should only make the change if the freebie
     belongs to the dev who's giving it away
--- 23-mock-cc-freebie-tracker/app/models/dev.rb
+++ 23-mock-cc-freebie-tracker/app/models/dev.rb
@@ -3,4 +3,22 @@ class Dev < ActiveRecord::Base
     has_many :freebies
     has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        # item = self.freebies.find_by(item_name: item_name)
+        # item ? true : false
+        # self.freebies.pluck(:item_name).include?(item_name)
+        self.freebies.any? {|f| f.item_name == item_name}
+    end
+
+    def give_away(dev, freebie)
+        # if self.received_one?(freebie.item_name)
+        #     freebie.update(dev: dev)
+        #     dev.reload
+        #     self.reload
+        # else
+        #     "You can't give something you don't own!"
+        # end
+        freebie.update(dev: dev) unless freebie.dev != self
+    end
+
 end

d75759509befbd7b49a4ea1e34d9d54be3a3264c
betalantz <betalantz@gmail.com>
2022-03-23 09:47:50 -0700

    adds comments to migration

--- 22-magazines-cc-practice/db/migrate/20220323152616_create_subscriptions.rb
+++ 22-magazines-cc-practice/db/migrate/20220323152616_create_subscriptions.rb
@@ -2,8 +2,10 @@ class CreateSubscriptions < ActiveRecord::Migration[5.2]
   def change
     create_table :subscriptions do |t|
       t.integer :price
+      # you can use belongs_to or references interchangeably
       t.belongs_to :reader
-      t.references :magazine
+      # it's also totally fine to set up column for the foreign key as just an integer column as below
+      t.integer :magazine_id
     end
   end
 end

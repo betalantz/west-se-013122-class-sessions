e347b207cd377b74e71ac1b9de24f798c5e66619
betalantz <betalantz@gmail.com>
2022-03-23 09:21:00 -0700

    completes Reader model

--- 22-magazines-cc-practice/app/models/reader.rb
+++ 22-magazines-cc-practice/app/models/reader.rb
@@ -2,5 +2,20 @@ class Reader < ActiveRecord::Base
 
     has_many :subscriptions
     has_many :magazines, through: :subscriptions
+
+    def subscribe(magazine, price)
+        # Subscription.create(magazine: magazine, price: price, reader: self)
+        self.subscriptions.create(magazine: magazine, price: price)
+    end
+
+    def total_subscription_price
+        self.subscriptions.sum(:price)
+    end
+
+    def cancel_subscription(magazine)
+        sub = self.subscriptions.find_by(magazine: magazine)
+        sub.destroy
+        # self.subscriptions.delete(sub)
+    end
   
 end
